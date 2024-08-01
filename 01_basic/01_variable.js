373238e19afdee987154c9e2c1340edd498897f3
IntejarAlam <73603266+IntejarAlam@users.noreply.github.com>
2024-08-01 21:23:02 +0000

    Container config done

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -2,6 +2,7 @@ const accountId = 143213
 let accountEmail = "Alam@gmail.com"
 var accountPassword = "4312"
 accountCity = "Jaipur"
+let accountState;
 // accountId = 2134
 
 //console.log(accountId)
@@ -15,4 +16,4 @@ Javascript is a safe language
 so without declier a variable we reserve memory for variable
 */
 
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
