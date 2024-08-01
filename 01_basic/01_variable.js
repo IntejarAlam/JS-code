79910099338698710dc2e35d03a34cf0a86c6bb6
IntejarAlam <73603266+IntejarAlam@users.noreply.github.com>
2024-08-01 21:09:27 +0000

    Container save done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 143213
+let accountEmail = "Alam@gmail.com"
+var accountPassword = "4312"
+accountCity = "Jaipur"
+// accountId = 2134
+
+//console.log(accountId)
+/*
+Prefer not to use var
+because of issu in block scope and functional scope
+*/
+
+/*
+Javascript is a safe language
+so without declier a variable we reserve memory for variable
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
