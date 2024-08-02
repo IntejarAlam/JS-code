b8d3b0306bcddf270104f98b280001c835c07736
IntejarAlam <73603266+IntejarAlam@users.noreply.github.com>
2024-08-02 13:46:08 +0000

    container config done

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as a newer version
+//alert(3+3) //we are useing node js not browser
+
+console.log(3
+    +3)  //code readiability should be high
+
+console.log("Alam")
+
+let name = "Alam"
+let age = 25
+let IsloggedIn = true
+
+//Data Type
+//number => 2 to power 53
+//bigint
+//string = "" 
+//boolean = true/false
+//null = standalone value
+//undefined
