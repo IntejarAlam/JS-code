7ff8a10c4b65669448d47eaef2d0d837de6ea406
IntejarAlam <73603266+IntejarAlam@users.noreply.github.com>
2024-08-03 20:10:03 +0000

    conversionOperation done

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,16 @@
+//let score = "30"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber))
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1, false => 0
+
+let IsloggedIn = ""
+let booleanIsLoggedIn = Boolean(IsloggedIn)
+console.log(booleanIsLoggedIn)
