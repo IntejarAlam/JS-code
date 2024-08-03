8276599ea2d8af85d41d11d518d37d380128372a
IntejarAlam <73603266+IntejarAlam@users.noreply.github.com>
2024-08-03 21:34:04 +0000

    Conversion Operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -13,4 +13,53 @@
 
 let IsloggedIn = ""
 let booleanIsLoggedIn = Boolean(IsloggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//1 => true
+//0 => false
+// "" => false
+//"Alam" => true
+
+let someNumber = 44
+let stringNumber = String(someNumber)
+
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//********************* OPERATIONS ***************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+//console.log(2+2)
+//console.log(4-2)
+//console.log(2*3)
+//console.log(2**3)
+//console.log(4/3)
+//console.log(5%2)
+
+let str1 = "Hi"
+let str2 = " Alam"
+let str3 = str1+str2
+//console.log(str3)
+
+// console.log("1"+2)  //not good
+// console.log(1 + "2")  //not good
+// console.log("1" + 2 + "2")  //not good
+// console.log(( 2 + 3 ) * 5 % 3)  //not good
+
+// console.log(+true)  //not good
+// console.log(+"")    //not good
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 //Not good
+//console.log(num1)
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+
+// Link to Study
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
