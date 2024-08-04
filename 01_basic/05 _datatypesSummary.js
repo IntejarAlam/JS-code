dd526efed4e11127c782f7897ae5e7b6e183ff2d
IntejarAlam <73603266+IntejarAlam@users.noreply.github.com>
2024-08-04 20:45:39 +0000

    datatypeSummary

---
+++ 01_basic/05 _datatypesSummary.js
@@ -0,0 +1,18 @@
+// ********** Primitive ********
+// 7 Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
+
+//***********Reference (Non Primitive)*********
+// Aray, Objects, Functions
