7a11b2c6ce1370d740571290b0eab392efe606e0
IntejarAlam <73603266+IntejarAlam@users.noreply.github.com>
2024-08-07 20:23:26 +0000

    Datatypes

--- 01_basic/05_datatypes_summary.js
+++ 01_basic/05_datatypes_summary.js
@@ -12,10 +12,10 @@ let userEmail
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id == anotherId)
+//console.log(id == anotherId)
 
 const bigNumber =  1263787872397837396n  //BigInt
-console.log(bigNumber)
+//console.log(bigNumber)
 
 //***********Reference (Non Primitive)*********
 // Array, Objects, Functions
@@ -29,6 +29,7 @@ let myObj = {
     age: "25",
 }
 
+
 //Function
 const myFunction = function(){
     console.log("Hello World");
@@ -36,3 +37,27 @@ const myFunction = function(){
 }
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// Stack(Premitive), Heap(Non-Primitive)
+
+//stack
+
+let myYoutubename = "Alamdotcom"
+let anothername = myYoutubename
+anothername = "Intejaraurcode"
+
+console.log(myYoutubename)
+console.log(anothername)
+
+//Heap
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "Alam@google.com" //In obj fetch variable by dot
+
+console.log(userOne)
+console.log(userTwo)
